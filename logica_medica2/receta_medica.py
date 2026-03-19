f1058c354346f437244474920c4ac131957a687b
Lucía Portal Uipan <lucia.portal07@gmail.com>
2026-03-19 16:19:41 -0500

    Update receta_medica.py

--- logica_medica2/receta_medica.py
+++ logica_medica2/receta_medica.py
@@ -24,18 +24,19 @@ class VentanaReceta(ctk.CTkToplevel):
         info_paciente = f"PACIENTE: {datos_paciente['nombre']}\nFECHA: {datos_paciente['fecha']}\nEDAD: {datos_paciente['edad']} años"
         ctk.CTkLabel(self.cuerpo, text=info_paciente, justify = "left", font=("Courier", 16, "bold"), text_color="black").pack(anchor="w", pady=10)
         
-        #reccomendacion titulo 
+        
         ctk.CTkLabel(self.cuerpo, text="PRESCRIPCION Y RECOMENDACIONES:", font=("Courier", 18, "underline", "bold"), text_color="black").pack(anchor="w", pady=10)
         #caja de texto de recomendacion
         self.txt_consejos = ctk.CTkTextbox(self.cuerpo, fg_color="white", text_color="black",font=("Courier", 14), border_color="#cccccc", border_width=1)
         self.txt_consejos.pack(fill="both", expand=True, pady=10)
         self.txt_consejos.insert("0.0", consejos)
         self.txt_consejos.configure(state="disabled")
-        #SELLO DE RIESGO 
+        
+        # sello peligro
         self.footer = ctk.CTkFrame(self.cuerpo, fg_color="transparent")
         self.footer.pack(fill="x", pady=20)
         
-        # Simulación de un sello circular
+        # sello circular
         self.sello = ctk.CTkFrame(self.footer, width=150, height=150, corner_radius=75, 
                                   border_width=3, border_color="#8B0000", fg_color="transparent")
         self.sello.pack(side="right", padx=20)
@@ -44,7 +45,7 @@ class VentanaReceta(ctk.CTkToplevel):
         ctk.CTkLabel(self.sello, text=f"RIESGO:\n{riesgo}%", font=("Courier", 20, "bold"), 
                      text_color="#8B0000").place(relx=0.5, rely=0.5, anchor="center")
 
-        # --- BOTONES DE ACCIÓN ---
+        # Botenes 
         self.btn_cerrar = ctk.CTkButton(self, text="VOLVER A EVALUAR", fg_color="#2c3e50",command=self.destroy) 
         self.btn_cerrar.pack(pady=20)
         
